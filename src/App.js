4df16b897c60c3c7db95fa503c309cf81a81be0c
bismayanuario <yanuariobisma23@gmail.com>
2022-11-28 13:27:03 +0700

    Register Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./component/Navbar/Navbar"
 import { Route, Routes } from "react-router-dom"
 import Footer from "./component/Footer/Footer"
 import Home from "./component/Home/Home"
+import Register from "./component/Register/Register"
 
 
 
@@ -18,7 +19,7 @@ function App() {
       <Routes>
         <Route>
             <Route path="/" element={<Home/>}/>
-
+            <Route path="/Register" element={<Register/>}/>
         </Route>
       </Routes>
       <Footer>
