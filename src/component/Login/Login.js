7bddf9d0286c6d7ac0b14edf29e0978732390fe7
bismayanuario <yanuariobisma23@gmail.com>
2022-12-02 12:56:32 +0700

    Update Login

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -6,17 +6,44 @@ import { useNavigate } from "react-router-dom";
 
 const Login = () =>{
     const navigate = useNavigate()
-    const [pass, setPass] = useState("")
-    const [email, setEmail] = useState("")
-    const userPass = localStorage.getItem("pass")
-    const userEmail = localStorage.getItem("email")
+    const [username, setUsername ] = useState("");
+    const [errorUsername,setErrorUsername] = useState('');
+    const [pass, setPass ] = useState("");
+    const [errorPass, setErrorPass] = useState('');
+    const [error, setError] = useState('');
+    const [errors, setErrors] = useState(false);
+    const userPass = localStorage.getItem("pass");
+    const userUsername = localStorage.getItem("username");
+
+    const onChangeUsername = (e) =>{
+        const value = e.target.value
+        setUsername(value)
+        setError('')
+        if(!value){
+            setErrorUsername('Username Tidak Boleh Kosong')
+        } else{
+            setErrorUsername('')
+        }
+    }
+    const onChangePassword = (e) =>{
+        const value = e.target.value
+        setPass(value)
+        setError('')
+        if(!value){
+            setErrorPass('Password tidak boleh kosong')
+        } else{
+            setErrorPass('')
+        }
+    }
 
     const handleSubmit = (e) =>{
         e.preventDefault();
-        if(email === userEmail && pass === userPass){
-            navigate('/')
+        if(username === userUsername && pass === userPass){
+            navigate('/Home')
             
             
+        } else if(username == 0 || pass == 0){
+            setErrors(true)
         }
         else{
             navigate('/Login')
@@ -27,13 +54,31 @@ const Login = () =>{
     return(
         <section class="section-login">
             <div class="container-login text-center">
+                {
+                    error && (
+                        <div className='alert alert-danger'>
+                            <p>{error}</p>
+                        </div>
+                    )
+                }
                 <div class="login">
                     <form action="" onSubmit={handleSubmit}>
                         <h1>Login</h1>
                         <button type="submit" class="btn btn-outline-secondary btn-sm" id="login-google"><FcGoogle style={{paddingRight:'0.5rem'}} size={30}/>Login With Google</button>
                         <p>Enter your Email and Password</p>
-                        <input type="email" placeholder="Email" id="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
-                        <input type="password" placeholder="Password" id="password" value={pass} onChange={(e)=> setPass(e.target.value)}/>
+                        <input type="text" placeholder="Username" id="username" required value={username} onChange={onChangeUsername}/>
+                        {
+                                                        errorUsername && (
+                                                            <p className="text-danger">{errorUsername}</p>
+                                                        )
+                                                    }
+                         <input type="password" placeholder="Password" id="password" required value={pass} onChange={onChangePassword}/>
+                        {
+                                                        errorPass && (
+                                                            <p className="text-danger">{errorPass}</p>
+                                                        )
+                                                    }
+                        
                         <p class="forgot-pass">
                             <a href="#">Forgot Password?</a>
                         </p>
